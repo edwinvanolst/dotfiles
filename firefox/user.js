30c0952a524f4ea8c5b0bd585a23aedb4a99c0c4
Edwin van Olst <e.vanolst@gmail.com>
2022-05-06 02:16:14 +0200

    firefox user.js

---
+++ firefox/user.js
@@ -0,0 +1,8 @@
+// fullscreen inside window
+user_pref("full-screen-api.ignore-widgets", true);
+// restart window size fix 
+user_pref("privacy.resistFingerprinting", true);
+// fix bspwm right click
+user_pref("ui.context_menus.after_mouseup", true);
+// disable geolocation
+user_pref("geo.enabled", false);
