e41f8a871e22ff9ecc020c83f64138b5bc3c4871
Edwin van Olst <e.vanolst@gmail.com>
2022-05-08 23:35:15 +0200

    firefox settings

--- firefox/user.js
+++ firefox/user.js
@@ -1,8 +1,16 @@
-// fullscreen inside window
-user_pref("full-screen-api.ignore-widgets", true);
-// restart window size fix 
-user_pref("privacy.resistFingerprinting", true);
+// ~/.mozilla/firefox/[PROFILE]/user.js
+
 // fix bspwm right click
 user_pref("ui.context_menus.after_mouseup", true);
+// fix bspwm window size at launch
+user_pref("privacy.resistFingerprinting", true);
+// fullscreen inside window
+user_pref("full-screen-api.ignore-widgets", true);
+
 // disable geolocation
 user_pref("geo.enabled", false);
+// disable telephony
+user_pref("dom.telephony.enabled", false);
+
+// close tabs with double click
+user_pref("browser.tabs.closeTabByDblclick", true);
